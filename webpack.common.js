02fd08a121e8b2ec09bf635aba84746de721a9da
Ali Salah <ali@salah.io>
2020-11-15 13:24:24 +0200

    remove run-tests

--- webpack.common.js
+++ webpack.common.js
@@ -15,13 +15,6 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new webpack.EnvironmentPlugin({
-      // Development Keys - See README.md
-      OAUTH_CLIENT_ID: '3fef4433a29c6ad8f22c',
-      OAUTH_CLIENT_SECRET: '9670de733096c15322183ff17ed0fc8704050379',
-    }),
-  ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
